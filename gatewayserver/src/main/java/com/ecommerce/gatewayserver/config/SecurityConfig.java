de364ec9515d5406b1368ef414339368cdae6b6f
Dauren Assanbaev <sharinganyt430@gmail.com>
2025-05-10 19:37:58 +0500

    added gitignore

--- gatewayserver/src/main/java/com/ecommerce/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/ecommerce/gatewayserver/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ecommerce.gatewayserver.config;
 
-import com.fasterxml.jackson.databind.ser.AnyGetterWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
